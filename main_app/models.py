998aa912f63d8aabd62c3551347b2e6f6ae9c616
Emily <echeun17@mail.ccsf.edu>
2019-09-10 10:17:47 -0700

    added CBV for create, update, delete

--- main_app/models.py
+++ main_app/models.py
@@ -1,6 +1,8 @@
 from django.db import models
-
+from django.urls import reverse
 # Create your models here.
+
+
 class Dog(models.Model):
     name = models.CharField(max_length=100)
     breed = models.CharField(max_length=100)
@@ -9,3 +11,6 @@ class Dog(models.Model):
 
     def __str__(self):
         return f'{self.name} {self.id}'
+
+    def get_absolute_url(self):
+        return reverse('detail', kwargs={'dog_id': self.id})
