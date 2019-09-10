c8b7e33af91eb19bb852178041ff498177fd2016
Emily <echeun17@mail.ccsf.edu>
2019-09-10 13:10:26 -0700

    added toys list

--- main_app/views.py
+++ main_app/views.py
@@ -1,22 +1,23 @@
 from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
-from .models import Dog
+from django.views.generic import ListView, DetailView
+from .models import Dog, Toy
 from .forms import FeedingForm
 
-#create home view
+# create home view
 def home(request):
     return render(request, 'home.html')
 
-#create about view
+# create about view
 def about(request):
     return render(request, 'about.html')
 
-#create dogs list view
+# create dogs list view
 def dogs_index(request):
     dogs = Dog.objects.all()
     return render(request, 'dogs/index.html', {'dogs': dogs})
 
-#create dogs detail view
+# create dogs detail view
 def dogs_detail(request, dog_id):
     dog = Dog.objects.get(id=dog_id)
     feeding_form = FeedingForm()
@@ -25,7 +26,23 @@ def dogs_detail(request, dog_id):
         'feeding_form': feeding_form
     })
 
-#add feeding view
+# class for CBV:create
+class DogCreate(CreateView):
+    model = Dog
+    fields = '__all__'
+    success_url = '/dogs/'
+
+# class for CBV:update
+class DogUpdate(UpdateView):
+    model = Dog
+    fields = ['breed', 'description']
+
+# class for CBV:delete
+class DogDelete(DeleteView):
+    model = Dog
+    success_url = '/dogs/'
+
+# add feeding view
 def add_feeding(request, dog_id):
     form = FeedingForm(request.POST)
     if form.is_valid():
@@ -34,20 +51,20 @@ def add_feeding(request, dog_id):
         new_feeding.save()
     return redirect('detail', dog_id=dog_id)
 
-#class for CBV:create
-class DogCreate(CreateView):
-    model = Dog
+class ToyList(ListView):
+    model = Toy
+
+class ToyDetail(DetailView):
+    model = Toy
+
+class ToyCreate(CreateView):
+    model = Toy
     fields = '__all__'
-    success_url = '/dogs/'
 
-#class for CBV:update
-class DogUpdate(UpdateView):
-    model = Dog
-    fields = ['breed', 'description']
+class ToyUpdate(UpdateView):
+    model = Toy
+    fields = ['name', 'color']
 
-#class for CBV:delete
-class DogDelete(DeleteView):
-    model = Dog
-    success_url = '/dogs/'
-
-
+class ToyDelete(DeleteView):
+    model = Toy
+    success_url = '/toys/'
