41e0d4b2cf4d9c2e186647b589e419b95435420a
Emily <echeun17@mail.ccsf.edu>
2019-09-10 11:46:40 -0700

    added toy model

--- main_app/models.py
+++ main_app/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.urls import reverse
+from datetime import date
 # Create your models here.
 
 MEALS = (
@@ -7,6 +8,16 @@ MEALS = (
     ('L', 'Lunch'),
     ('D', 'Dinner')
 )
+
+class Toy(models.Model):
+    name = models.CharField(max_length=50)
+    color = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('toys_detail', kwargs={'pk': self.id})
 
 class Dog(models.Model):
     name = models.CharField(max_length=100)
@@ -19,6 +30,9 @@ class Dog(models.Model):
 
     def get_absolute_url(self):
         return reverse('detail', kwargs={'dog_id': self.id})
+    
+    def fed_for_today(self):
+        return len(self.feeding_set.filter(date=date.today())) >= len(MEALS)
 
 class Feeding(models.Model):
     date = models.DateField('feeding date')
@@ -33,4 +47,5 @@ class Feeding(models.Model):
     def __str__(self):
         return f'{self.get_meal_display()} on {self.date}'
 
-
+    class Meta:
+        ordering = ['-date']
